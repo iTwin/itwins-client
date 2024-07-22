89b8285c48dae783748ad638222f12aee8f0b092
David Bryant <David.Bryant@bentley.com>
2024-07-22 13:11:26 -0400

    Fix tests

--- src/test/integration/iTwinsClient.test.ts
+++ src/test/integration/iTwinsClient.test.ts
@@ -442,9 +442,7 @@ describe("iTwinsClient", () => {
 
     iTwins.forEach((actualiTwin) => {
       chai.expect(actualiTwin.parentId).to.be.a("string");
-      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");
-      chai.expect(actualiTwin.imageName).to.be.a("string").or.null;
-      chai.expect(actualiTwin.image).to.be.a("string").or.null;
+      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");     
       chai.expect(actualiTwin.createdDateTime).to.be.a("string");
       chai.expect(actualiTwin.createdBy).to.be.a("string");
     });
@@ -500,9 +498,7 @@ describe("iTwinsClient", () => {
     // All items have the field "createdDateTime"
     iTwins.forEach((actualiTwin) => {
       chai.expect(actualiTwin.parentId).to.be.a("string");
-      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");
-      chai.expect(actualiTwin.imageName).to.be.a("string").or.null;
-      chai.expect(actualiTwin.image).to.be.a("string").or.null;
+      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");      
       chai.expect(actualiTwin.createdDateTime).to.be.a("string");
       chai.expect(actualiTwin.createdBy).to.be.a("string");
     });
@@ -539,9 +535,7 @@ describe("iTwinsClient", () => {
 
     iTwinsResponse.data!.forEach((actualiTwin) => {
       chai.expect(actualiTwin.parentId).to.be.a("string");
-      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");
-      chai.expect(actualiTwin.imageName).to.be.a("string").or.null;
-      chai.expect(actualiTwin.image).to.be.a("string").or.null;
+      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");      
       chai.expect(actualiTwin.createdDateTime).to.be.a("string");
       chai.expect(actualiTwin.createdBy).to.be.a("string");
     });
@@ -585,9 +579,7 @@ describe("iTwinsClient", () => {
 
     iTwinsResponse.data!.forEach((actualiTwin) => {
       chai.expect(actualiTwin.parentId).to.be.a("string");
-      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");
-      chai.expect(actualiTwin.imageName).to.be.a("string").or.null;
-      chai.expect(actualiTwin.image).to.be.a("string").or.null;
+      chai.expect(actualiTwin.iTwinAccountId).to.be.a("string");      
       chai.expect(actualiTwin.createdDateTime).to.be.a("string");
       chai.expect(actualiTwin.createdBy).to.be.a("string");
     });
